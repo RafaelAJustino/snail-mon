6241de91575cde4fadd433b717a1ee37db0c20d2
RafaelAJustino <rafael.angelo.justino@gmail.com>
2023-03-07 20:13:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,832 @@
+const bracos = [
+    {
+        id: 0,
+        img: '#f1cfb3',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 1,
+        img: '#5386e4',
+        hp: 3,
+        atk: 3 * 4,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 2,
+        img: '#6A5ACD',
+        hp: 3,
+        atk: 3 * 3,
+        sAtk: 3 * 2,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 3,
+        img: '#191970',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3 * 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 4,
+        img: '#708090',
+        hp: 3,
+        atk: 3 * 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 5,
+        img: '#00FFFF',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 6,
+        img: '#7FFFD4',
+        hp: 3,
+        atk: 3 * 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 7,
+        img: '#00FA9A',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 3,
+        vel: 3,
+    },
+    {
+        id: 8,
+        img: '#006400',
+        hp: 3,
+        atk: 3,
+        sAtk: 3 * 4,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 9,
+        img: '#9ACD32',
+        hp: 3,
+        atk: 3,
+        sAtk: 3 * 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 10,
+        img: '#DAA520',
+        hp: 3,
+        atk: 3,
+        sAtk: 3 * 2,
+        def: 3 * 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 11,
+        img: '#8B4513',
+        hp: 3,
+        atk: 3,
+        sAtk: 3 * 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 12,
+        img: '#CD853F',
+        hp: 3,
+        atk: 3,
+        sAtk: 3 * 2,
+        def: 3,
+        sDef: 3 * 3,
+        vel: 3,
+    },
+    {
+        id: 13,
+        img: '#FFDEAD',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 4,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 14,
+        img: '#F5DEB3',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 15,
+        img: '#4B0082',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 3,
+        vel: 3,
+    },
+    {
+        id: 16,
+        img: '#DC143C',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 4,
+        vel: 3,
+    },
+    {
+        id: 17,
+        img: '#8B0000',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3 * 2,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 18,
+        img: '#FFFF00',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3 * 2,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 19,
+        img: '#FF0000',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 20,
+        img: '#FF4500',
+        hp: 3,
+        atk: 3,
+        sAtk: 3 * 2,
+        def: 3 * 2,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+];
+
+const peles = [
+    {
+        id: 0,
+        img: '#f1cfb3',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 1,
+        img: '#5386e4',
+        hp: 3 * 4,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 2,
+        img: '#6A5ACD',
+        hp: 3 * 3,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 3,
+        img: '#191970',
+        hp: 3 * 2,
+        atk: 3 * 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 4,
+        img: '#708090',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3 * 2,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 5,
+        img: '#00FFFF',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3 * 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 6,
+        img: '#7FFFD4',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 7,
+        img: '#00FA9A',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 8,
+        img: '#006400',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 9,
+        img: '#9ACD32',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 3,
+        vel: 3,
+    },
+    {
+        id: 10,
+        img: '#DAA520',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 11,
+        img: '#8B4513',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 12,
+        img: '#CD853F',
+        hp: 3 * 2,
+        atk: 3 * 2,
+        sAtk: 3 * 2,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 13,
+        img: '#FFDEAD',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 14,
+        img: '#F5DEB3',
+        hp: 3 * 2,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 15,
+        img: '#4B0082',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3 * 2,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 16,
+        img: '#DC143C',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 17,
+        img: '#8B0000',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3 * 2,
+    },
+];
+
+const cascos = [
+    {
+        id: 0,
+        img: '#f1cfb3',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 1,
+        img: '#5386e4',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 4,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 2,
+        img: '#6A5ACD',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 3,
+        img: '#191970',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 4,
+        img: '#708090',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 5,
+        img: '#00FFFF',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 3,
+        vel: 3,
+    },
+    {
+        id: 6,
+        img: '#7FFFD4',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 7,
+        img: '#00FA9A',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 8,
+        img: '#006400',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 4,
+    },
+    {
+        id: 9,
+        img: '#9ACD32',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 10,
+        img: '#DAA520',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 11,
+        img: '#8B4513',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3 * 3,
+    },
+    {
+        id: 12,
+        img: '#CD853F',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 13,
+        img: '#FFDEAD',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 4,
+        vel: 3,
+    },
+    {
+        id: 14,
+        img: '#F5DEB3',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 2,
+        vel: 3,
+    },
+    {
+        id: 15,
+        img: '#4B0082',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 2,
+        vel: 3 * 2,
+    },
+    {
+        id: 16,
+        img: '#DC143C',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 17,
+        img: '#8B0000',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3 * 2,
+    },
+];
+
+const bases = [
+    {
+        id: 0,
+        img: '#f1cfb3',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3,
+    },
+    {
+        id: 1,
+        img: '#5386e4',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 4,
+    },
+    {
+        id: 2,
+        img: '#6A5ACD',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 3,
+        img: '#191970',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 4,
+        img: '#708090',
+        hp: 3 * 2,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 5,
+        img: '#00FFFF',
+        hp: 3 * 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 6,
+        img: '#7FFFD4',
+        hp: 3,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 7,
+        img: '#00FA9A',
+        hp: 3,
+        atk: 3 * 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 8,
+        img: '#006400',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3,
+        vel: 3 * 3,
+    },
+    {
+        id: 9,
+        img: '#9ACD32',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 10,
+        img: '#DAA520',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 2,
+        vel: 3 * 3,
+    },
+    {
+        id: 11,
+        img: '#8B4513',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3,
+        sDef: 3 * 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 12,
+        img: '#CD853F',
+        hp: 3 * 2,
+        atk: 3 * 2,
+        sAtk: 3,
+        def: 3,
+        sDef: 3,
+        vel: 3 * 2,
+    },
+    {
+        id: 13,
+        img: '#FFDEAD',
+        hp: 3,
+        atk: 3,
+        sAtk: 3,
+        def: 3 * 2,
+        sDef: 3 * 2,
+        vel: 3 * 2,
+    },
+];
+
+let br = bracos[0];
+let pe = peles[0];
+let ca = cascos[0];
+let ba = bases[0];
+
+function statusSnail(braco, pele, casco, base) {
+    const hp = document.getElementById('hp');
+    const atk = document.getElementById('atk');
+    const sAtk = document.getElementById('satk');
+    const def = document.getElementById('def');
+    const sDef = document.getElementById('sdef');
+    const vel = document.getElementById('vel');
+
+    hp.innerHTML = braco.hp + pele.hp + casco.hp + base.hp;
+    atk.innerHTML = braco.atk + pele.atk + casco.atk + base.atk;
+    sAtk.innerHTML = braco.sAtk + pele.sAtk + casco.sAtk + base.sAtk;
+    def.innerHTML = braco.def + pele.def + casco.def + base.def;
+    sDef.innerHTML = braco.sDef + pele.sDef + casco.sDef + base.sDef;
+    vel.innerHTML = braco.vel + pele.vel + casco.vel + base.vel;
+}
+
+function changeBraco(value) {
+    const braco = document.getElementById('braco');
+    braco.style.backgroundColor = bracos[value].img;
+    br = bracos[value];
+    statusSnail(br, pe, ca, ba);
+}
+
+function changePele(value) {
+    const pele = document.getElementById('pele');
+    pele.style.borderBottom = `150px solid ${peles[value].img}`;
+    pe = peles[value];
+    statusSnail(br, pe, ca, ba);
+}
+
+function changeCasco(value) {
+    const casco = document.getElementById('casco');
+    casco.style.backgroundColor = cascos[value].img;
+    ca = cascos[value];
+    statusSnail(br, pe, ca, ba);
+}
+
+function changeBase(value) {
+    const base = document.getElementById('base');
+    base.style.backgroundColor = bases[value].img;
+    ba = bases[value];
+    statusSnail(br, pe, ca, ba);
+}
+
+function creatBtn(value, func) {
+    const btn = document.createElement('button');
+    btn.value = value.id;
+    btn.className = 'btns';
+    btn.innerText = value.id;
+    btn.title = 
+    `
+HP: ${value.hp} 
+ATK: ${value.atk} 
+S.ATK: ${value.sAtk}
+DEF: ${value.sDef} 
+S.DEF: ${value.def} 
+VEL: ${value.vel}
+    `
+
+
+    switch (func) {
+        case 'changeBraco':
+            btn.addEventListener('click', () => { changeBraco(value.id) });
+            return btn;
+        case 'changePele':
+            btn.addEventListener('click', () => { changePele(value.id) });
+            return btn;
+        case 'changeCasco':
+            btn.addEventListener('click', () => { changeCasco(value.id) });
+            return btn;
+        case 'changeBase':
+            btn.addEventListener('click', () => { changeBase(value.id) });
+            return btn;
+    }
+
+}
+
+function loadOpts() {
+    const bracosOpts = document.getElementById('bracos_opts');
+    const pelesOpts = document.getElementById('peles_opts');
+    const cascosOpts = document.getElementById('cascos_opts');
+    const basesOpts = document.getElementById('bases_opts');
+
+
+    for (const x of bracos) {
+        bracosOpts.appendChild(creatBtn(x, 'changeBraco'));
+    }
+
+    for (const x of peles) {
+        pelesOpts.appendChild(creatBtn(x, 'changePele'));
+    }
+
+    for (const x of cascos) {
+        cascosOpts.appendChild(creatBtn(x, 'changeCasco'));
+    }
+
+    for (const x of bases) {
+        basesOpts.appendChild(creatBtn(x, 'changeBase'));
+    }
+}
+
+window.onload = () => {
+    loadOpts();
+    statusSnail(br, pe, ca, ba);
+}
